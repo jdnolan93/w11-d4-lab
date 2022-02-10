e015a25e1e9b0d2803f4b305dcafdecd4a83c925
Jamie <jamienolan93@hotmail.co.uk>
2022-02-10 12:02:59 +0000

    conference room test has name created and passed

--- src/main/java/Room/ConferenceRoom.java
+++ src/main/java/Room/ConferenceRoom.java
@@ -1,4 +1,18 @@
 package Room;
 
-public class ConferenceRoom {
+public class ConferenceRoom extends Room {
+
+    String name;
+    public ConferenceRoom(int capacity, String name) {
+        super(capacity);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,6 +1,20 @@
 import Room.ConferenceRoom;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ConferenceRoomTest {
 
     ConferenceRoom conferenceRoom;
+
+    @Before
+    public void before(){
+        conferenceRoom = new ConferenceRoom(200, "Le Grande");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Le Grande", conferenceRoom.getName());
+    }
 }

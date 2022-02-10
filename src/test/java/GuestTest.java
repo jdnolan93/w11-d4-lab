13b67afb8b8b59831e2762a4a8524779eac9d0bd
Jamie <jamienolan93@hotmail.co.uk>
2022-02-10 10:26:58 +0000

    setup guest test created and passed for hasName

---
+++ src/test/java/GuestTest.java
@@ -0,0 +1,22 @@
+import Guest.Guest;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GuestTest {
+
+    Guest guest;
+
+    @Before
+    public void before(){
+        guest = new Guest("John Smith");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("John Smith", guest.getName());
+    }
+
+
+}
